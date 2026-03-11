a1ed1a493a883f671b14b81db6b6469e0cb40c72
Opris Candid <candid.opris@ulbsibiu.ro>
2026-03-11 09:39:30 +0200

    -Student list -grammar correction

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,13 +1,33 @@
 package ro.ulbs.proiectaresoftware.students;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     static void main() {
-        Student s1 = new Student(112, "Ioan","Popa","T121/1");
-        Student s2 = new Student(112, "Maria","Oprea","T121/1");
-        Student s3 = new Student(120, "Alis","Popa","T121/2");
-        Student s4 = new Student(122, "Mihai","Vecerdea","T122/1");
-        Student s5 = new Student(122, "Eugen","Uritescu","T122/2");
-        System.out.println(s1);
+        Student s1 = new Student(112, "Ioan","Popa","TI21/1");
+        Student s2 = new Student(112, "Maria","Oprea","TI21/1");
+        Student s3 = new Student(120, "Alis","Popa","TI21/2");
+        Student s4 = new Student(122, "Mihai","Vecerdea","TI22/1");
+        Student s5 = new Student(122, "Eugen","Uritescu","TI22/2");
 
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println(s5);
+
+        List<Student> studenti = new ArrayList();
+        System.out.println("Nr. studenti este " + studenti.size());
+        studenti.add(s1);
+        studenti.add(s2);
+        studenti.add(s3);
+        studenti.add(s4);
+        studenti.add(s5);
+        System.out.println("Nr. studenti este " + studenti.size());
+
+        for (Student s : studenti){
+            System.out.println(s);
+        }
     }
 }
