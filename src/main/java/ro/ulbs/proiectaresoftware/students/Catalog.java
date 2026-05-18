d40bde508bbdd92ba1a30942f9f5240cc78c683c
Opris Candid <candid.opris@ulbsibiu.ro>
2026-05-18 12:46:11 +0300

    -new classes Catalog, StudentRecord

---
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Catalog.java
@@ -0,0 +1,87 @@
+package ro.ulbs.proiectaresoftware.students;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Catalog {
+    private static final Catalog instance = new Catalog();
+    private Catalog() {
+    }
+
+    public static Catalog getInstance() {
+        return instance;
+    }
+
+    private static Exporter getExporterToFile(String filename) {
+        String fileExtension = filename.substring(filename.lastIndexOf('.'));
+        return switch (fileExtension) {
+            case ".xlsx" -> new ExportToExcel(filename);
+            case ".csv" -> new ExportToCSV(filename);
+            case ".txt" -> new ExportToText(filename);
+            default -> throw new IllegalArgumentException("Extensie nesuportata " + fileExtension);
+        };
+    }
+
+    private static void exportList(List<Student> list, Exporter exporter) {
+        exporter.export_studenti(list);
+    }
+
+    public static Map<Student, Integer> noteStudentiFaraMatricol(List<Student> list, Map<String, Integer> n) {
+        Map<Student, Integer> notaFinal = new HashMap<>();
+        for (Student s : list) {
+            Integer nota = n.get(s.getNumarMatricol());
+            if (nota != null) {
+                notaFinal.put(s, nota);
+            }
+        }
+        return notaFinal;
+    }
+
+    public static boolean prezenta(Collection<Student> c, Student s) {
+        return c.contains(s);
+    }
+
+    public static Integer nota(Map<String, Integer> note, Student student) {
+        return note.get(student.getNumarMatricol());
+    }
+
+    public static Map<String, Integer> citireNote(String csv) {
+        Map<String, Integer> mapCreat = new HashMap<>();
+        try {
+            File file = new File(csv);
+            Scanner scn = new Scanner(file);
+            while (scn.hasNextLine()) {
+                String line = scn.nextLine();
+                String[] date = line.split(",");
+                if (date.length == 2) {
+                    String nume = date[0].trim();
+                    Integer valoare = Integer.parseInt(date[1].trim());
+                    mapCreat.put(nume, valoare);
+                }
+            }
+            scn.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Nu s-a gasit fisierul!");
+            e.printStackTrace();
+        }
+        return mapCreat;
+    }
+
+    public static void sortareDupaNumeSiFormatiune(List<Student> list) {
+        Collections.sort(list, new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                if (o1.formatieDeStudiu.equals(o2.formatieDeStudiu)) {
+                    return o1.nume.compareTo(o2.nume);
+                }
+                return o1.formatieDeStudiu.compareTo(o2.formatieDeStudiu);
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return false;
+            }
+        });
+    }
+}
---
+++ src/main/java/ro/ulbs/proiectaresoftware/students/StudentRecord.java
@@ -0,0 +1,44 @@
+package ro.ulbs.proiectaresoftware.students;
+
+import java.util.Objects;
+
+public record StudentRecord(
+        String numarMatricol,
+        String prenume,
+        String nume,
+        String formatieDeStudiu
+) implements Comparable<Student> {
+
+    @Override
+    public String toString() {
+        return String.format("%s %10s %10s %10s", numarMatricol, prenume, nume, formatieDeStudiu);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(prenume, student.prenume) &&
+                Objects.equals(nume, student.nume) &&
+                Objects.equals(formatieDeStudiu, student.formatieDeStudiu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(prenume, nume, formatieDeStudiu);
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        if (this.formatieDeStudiu.equals(s.formatieDeStudiu)) {
+            if (this.nume.equals(s.nume)) {
+                return this.prenume.compareTo(s.prenume);
+            }
+            return this.nume.compareTo(s.nume);
+        }
+        return this.formatieDeStudiu.compareTo(s.formatieDeStudiu);
+    }
+
+    
+}
